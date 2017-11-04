083279f1c1530556887cdc960908872cdfafef5b
y-nakazawa <y.nakazawa1220@gmail.com>
2017-11-05 00:05:24 +0900

    任意のPeerIDを指定できるように対応した

--- script.js
+++ script.js
@@ -14,25 +14,38 @@ navigator.mediaDevices.getUserMedia({video:true, audio:true})
         return;
     });
 
-// Create peer object.
-peer = new Peer({
-    key: '55ed50bb-74ed-46cf-b08d-3486b05b9f47',
-    debug: 3
-});    
+$(function(){
+    $('#make-call').children().prop("disabled", true);    
+});
 
-peer.on('open', function(){
-    // Connect to signaling server.
-    $('#my-id').text(peer.id);
-}).on('error', function(err){
-    // Errot
-    alert(err.message);
-}).on('close', function(){
-    // Close 
-}).on('disconnected', function(){
-    // Disonnect to signaling server.
-}).on('call', function(call){
-    call.answer(localStream);
-    setupCallEventHandlers(call);
+// Peering
+$('#make-peer').submit(function(e){
+    e.preventDefault();
+    var peerId = $('#peer-id').val();
+
+    // Create peer object.
+    peer = new Peer(peerId,        
+        {
+        key: '55ed50bb-74ed-46cf-b08d-3486b05b9f47',
+        debug: 3
+    });    
+
+    peer.on('open', function(){
+        // Connect to signaling server.
+        $('#my-id').text(peer.id);
+    }).on('error', function(err){
+        // Errot
+        alert(err.message);
+    }).on('close', function(){
+        // Close 
+    }).on('disconnected', function(){
+        // Disonnect to signaling server.
+    }).on('call', function(call){
+        call.answer(localStream);
+        setupCallEventHandlers(call);
+    });
+    
+    $('#make-call').children().prop("disabled", false);        
 });
 
 // Calling
@@ -44,6 +57,7 @@ $('#make-call').submit(function(e){
 
 $('#end-call').click(function(){
     existingCall.close();
+    $('#make-call').children().prop("disabled", true);            
 });
 
 function setupCallEventHandlers(call) {
